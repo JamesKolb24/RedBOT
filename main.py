a77d3f8189d5df26c8226d457d2217056e349b2b
jkolb024 <jkolb024@gmail.com>
2024-12-31 01:53:39 -0600

    Started on main.py and added text file for token

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# Import resources from discord
+from discord_components import ComponentsBot
 
+# Creat bot object
+bot = ComponentsBot()
+
+with open("token.txt","r") as file:
+    token = file.readlines()[0]
+bot.run(token)
